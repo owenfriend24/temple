b5b3bd39e049dbefbc90160ec31f879fb7015835
Owen Friend <ofriend@login2.ls6.tacc.utexas.edu>
2025-02-22 15:12:11 -0600

    ashs update

---
+++ bin/copy_to_scratch_ashs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: copy_ashs.sh ashs_dir subject"
+
+    exit 1
+fi
+
+ashs_dir=$1
+sub=$2
+mkdir ${ashs_dir}/sub-${sub}
+
+cp /corral-repl/utexas/prestonlab/temple/sub-${sub}/anat/sub-${sub}_desc_preproc_T2w.nii.gz ${ashs_dir}/sub-${sub}/
+
+cp /corral-repl/utexas/prestonlab/temple/sub-${sub}/anat/sub-${sub}_T1w_ss.nii.gz ${ashs_dir}/sub-${sub}/
--- bin/run_ashs.sh
+++ bin/run_ashs.sh
@@ -14,4 +14,4 @@ export ASHS_ROOT=/scratch/09123/ofriend/ashs
 #$ASHS_ROOT/bin/ashs_main.sh -L -I sub-temple108 -a $CORR/ASHS_DG_CA23_combined/ -g $FM/sub-temple108/anat/sub-temple108_T1w_ss.nii.gz -f $FM/../../sub-temple108/anat/coronal_prep.nii.gz -w $FM/sub-temple108/left_only/
 
 
-$ASHS_ROOT/bin/ashs_main.sh -I sub-${sub} -a $ASHS_ROOT/test_atlas/ -g $ASHS_ROOT/new/sub-${sub}/sub-${sub}_T1w_ss.nii.gz -f $ASHS_ROOT/sub-${sub}/new/sub-${sub}_desc-preproc_T2w.nii.gz -w $ASHS_ROOT/new/sub-${sub}/
+$ASHS_ROOT/bin/ashs_main.sh -I sub-${sub} -a $ASHS_ROOT/test_atlas/ -g $ASHS_ROOT/sub-${sub}/sub-${sub}_T1w_ss.nii.gz -f $ASHS_ROOT/sub-${sub}/sub-${sub}_desc-preproc_T2w.nii.gz -w $ASHS_ROOT/sub-${sub}/
