8cbb645bef05de3c136c2384fde7c1f667af3fce
owenfriend24 <owenwfriend@gmail.com>
2025-02-26 12:28:03 -0600

    trouble shoot prep_func_data.sh

--- bin/prep_func_data.sh
+++ bin/prep_func_data.sh
@@ -18,6 +18,7 @@ source /home1/09123/ofriend/analysis/temple/profile
 # Run your Python script
 
 python /home1/09123/ofriend/analysis/temple/bin/prep_func_data.py "${fsdir}" "${fmdir}" "${subject}"
+echo "ran python script"
 #mni_transforms.sh $fmdir $subject
 #temple_smooth.sh $fmdir $fsdir $subject "collector"
 #temple_smooth.sh $fmdir $fsdir $subject "arrow"
