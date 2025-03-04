665e59e994f4862cffc80960d231d0095c3cdf59
owenfriend24 <owenwfriend@gmail.com>
2025-03-04 12:49:04 -0600

    testing aggregate integration function

--- bin/batch_agg.sh
+++ bin/batch_agg.sh
@@ -5,19 +5,19 @@ comparison=$1
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_hip_subregions --age_file=True
+AB b_hip_subregions --agg_file=True
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_ifg_subregions --age_file=True
+AB b_ifg_subregions --agg_file=True
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_hip_subregions --age_file=True
+AB b_hip_subregions --agg_file=True
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_ifg_subregions --age_file=True
+AB b_ifg_subregions --agg_file=True
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC b_hip_subregions --age_file=True
+BC b_hip_subregions --agg_file=True
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC b_ifg_subregions --age_file=True
+BC b_ifg_subregions --agg_file=True
