b9e575fd1d66bbc314d82f66b3b46e92549f382f
Owen Friend <ofriend@login2.ls6.tacc.utexas.edu>
2025-03-04 16:31:04 -0600

    aggregate run

--- bin/batch_agg.sh
+++ bin/batch_agg.sh
@@ -5,19 +5,19 @@ comparison=$1
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_hip_subregions --agg_file=True
+AB b_hip_subregions --agg_file
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_ifg_subregions --agg_file=True
+AB b_ifg_subregions --agg_file
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_hip_subregions --agg_file=True
+AC b_hip_subregions --agg_file
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_ifg_subregions --agg_file=True
+AC b_ifg_subregions --agg_file
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC b_hip_subregions --agg_file=True
+BC b_hip_subregions --agg_file
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC b_ifg_subregions --agg_file=True
+BC b_ifg_subregions --agg_file
--- bin/test_coronal.sh
+++ bin/test_coronal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $# -lt 1]]; then
+if [[ $# -lt 1 ]]; then
     echo "Usage: test_coronal.sh subject"
 
     exit 1
@@ -8,6 +8,6 @@ fi
 
 sub=$1
 
-N4BiasFieldCorrection -i $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w.nii.gz $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_corr.nii.gz
+N4BiasFieldCorrection -i $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w.nii.gz -o $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_corr.nii.gz
 
 bet $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_corr.nii.gz $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_coronal.nii.gz -f 0.01
