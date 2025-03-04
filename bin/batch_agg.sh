ea91d3863ffee8bc444b9674fbeb10d8a89f43d8
owenfriend24 <owenwfriend@gmail.com>
2025-03-04 16:56:26 -0600

    debuggin aggregate

--- bin/batch_agg.sh
+++ bin/batch_agg.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-comparison=$1
+subject=$1
+
 
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
-aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_hip_subregions --agg_file
-
-aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB b_ifg_subregions --agg_file
+#aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
+#AB b_hip_subregions --agg_file
+#
+#aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
+#AB b_ifg_subregions --agg_file
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
 AC b_hip_subregions --agg_file
@@ -16,8 +17,8 @@ AC b_hip_subregions --agg_file
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
 AC b_ifg_subregions --agg_file
 
-aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC b_hip_subregions --agg_file
-
-aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC b_ifg_subregions --agg_file
+#aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
+#BC b_hip_subregions --agg_file
+#
+#aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
+#BC b_ifg_subregions --agg_file
