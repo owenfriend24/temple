2ad355da9c2727e25f7a92377ab2049e16d2dc6a
Owen Friend <ofriend@login2.ls6.tacc.utexas.edu>
2025-03-07 15:00:09 -0600

    updated test_coronal to prep_coronal

--- bin/prep_coronal.sh
+++ bin/prep_coronal.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 if [[ $# -lt 1 ]]; then
-    echo "Usage: test_coronal.sh subject"
+    echo "Usage: prep_coronal.sh subject"
 
     exit 1
 fi
 
 sub=$1
 
+
 N4BiasFieldCorrection -i $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w.nii.gz -o $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_corr.nii.gz
 
 bet $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_corr.nii.gz $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_coronal.nii.gz -f 0.01
+
+mkdir -p $SCRATCH/ashs/new_test/sub-${sub}
+
+cp $SCRATCH/temple/new_prepro/sub-${sub}/anat/sub-${sub}_T2w_coronal.nii.gz $SCRATCH/ashs/new_test/sub-${sub}/coronal.nii.gz
+
+cp $FM/sub-${sub}/anat/sub-${sub}_T1w_ss.nii.gz $SCRATCH/ashs/new_test/sub-${sub}/highres.nii.gz
