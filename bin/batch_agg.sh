e55b2af6f84ce937314b016a0a4c2b09c7758e2d
owenfriend24 <owenwfriend@gmail.com>
2025-03-12 18:20:22 -0500

    batch agg 2

--- bin/batch_agg.sh
+++ bin/batch_agg.sh
@@ -5,13 +5,13 @@ subject=$1
 
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
-#aggregate_integration.py both /corral-repl/utexas/temple/integration_prepost \
+#aggregate_integration.py both /corral-repl/utexas/prestonlab/temple/integration_prepost \
 #AB hip_subfields --agg_file
 
 #aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
 #AB b_ifg_subregions --agg_file
 
-aggregate_integration.py both /corral-repl/utexas/temple/integration_prepost \
+aggregate_integration.py both /corral-repl/utexas/prestonlab/temple/integration_prepost \
 AC hip_subfields --agg_file
 
 #aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
