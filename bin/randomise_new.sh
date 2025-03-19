f5b1d82c9583426f94c1077d936944b16f4c2b16
owenfriend24 <owenwfriend@gmail.com>
2025-03-19 12:11:51 -0500

    AB sl

--- bin/randomise_new.sh
+++ bin/randomise_new.sh
@@ -19,16 +19,16 @@ randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/group_z.nii
 -m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
 -n 5000 -x  --uncorrp
 
-randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/child_group_z.nii.gz \
--o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/child_parametric_age_ \
--d ${fmriprep_dir}/integration_prepost/randomise/age_param_child.mat \
--t ${fmriprep_dir}/integration_prepost/randomise/age_param_child.con \
--m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
--n 5000 -x  --uncorrp
-
-
-randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/adult_group_z.nii.gz \
--o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/adult_z_ \
--m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
--1 \
--n 5000 -x  --uncorrp
+#randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/child_group_z.nii.gz \
+#-o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/child_parametric_age_ \
+#-d ${fmriprep_dir}/integration_prepost/randomise/age_param_child.mat \
+#-t ${fmriprep_dir}/integration_prepost/randomise/age_param_child.con \
+#-m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
+#-n 5000 -x  --uncorrp
+#
+#
+#randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/adult_group_z.nii.gz \
+#-o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/adult_z_ \
+#-m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
+#-1 \
+#-n 5000 -x  --uncorrp
