22afb4fbcb214e8ca9f6967a42888a64a18c0206
owenfriend24 <owenwfriend@gmail.com>
2025-03-20 12:27:01 -0500

    BC symm vals

--- bin/batch_agg.sh
+++ bin/batch_agg.sh
@@ -5,7 +5,7 @@ subject=$1
 
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
-aggregate_integration.py both /corral-repl/utexas/prestonlab/temple/integration_prepost \
+aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
 BC lat_hip_subregions --agg_file
 #
 #aggregate_integration.py both /corral-repl/utexas/prestonlab/temple/integration_prepost \
