7fd02487ec48ab1779373b3e949b6105a07283ab
owenfriend24 <owenwfriend@gmail.com>
2025-03-21 10:27:36 -0500

    adjacent sl

--- bin/randomise_new.sh
+++ bin/randomise_new.sh
@@ -10,7 +10,7 @@ fi
 fmriprep_dir=$1
 comp=$2
 
-
+mkdir -p /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_${comp}/prepost/randomise_out/
 #randomise -i ${fmriprep_dir}/integration_prepost/mni_AB/prepost/group_z_image.nii.gz \
 #-o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_AB/prepost/randomise_out/grouped_age_new \
 #-d ${fmriprep_dir}/integration_prepost/randomise/age_grouped.mat \
@@ -18,8 +18,8 @@ comp=$2
 #-m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
 #-n 5000 -x --uncorrp
 
-randomise -i ${fmriprep_dir}/integration_prepost/mni_AC_shuffle/prepost/group_z_image.nii.gz \
--o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_AC_shuffle/prepost/randomise_out/grouped_age_new \
+randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/group_z_image.nii.gz \
+-o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_ABC/prepost/randomise_out/grouped_age_new \
 -d ${fmriprep_dir}/integration_prepost/randomise/age_grouped.mat \
 -t /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/age_grouped_chi_new.con \
 -m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
@@ -27,12 +27,12 @@ randomise -i ${fmriprep_dir}/integration_prepost/mni_AC_shuffle/prepost/group_z_
 
 #-o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/grouped_age_new \
 
-#randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/child_group_z.nii.gz \
-#-o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/child_parametric_age_ \
-#-d ${fmriprep_dir}/integration_prepost/randomise/age_param_child.mat \
-#-t ${fmriprep_dir}/integration_prepost/randomise/age_param_child.con \
-#-m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
-#-n 5000 -x  --uncorrp
+randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/child_group_z.nii.gz \
+-o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_${comp}/prepost/randomise_out/child_parametric_age_ \
+-d ${fmriprep_dir}/integration_prepost/randomise/age_param_child.mat \
+-t ${fmriprep_dir}/integration_prepost/randomise/age_param_child.con \
+-m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
+-n 5000 -x  --uncorrp
 #
 #
 #randomise -i ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/adult_group_z.nii.gz \
