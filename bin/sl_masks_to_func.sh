af9a196e82c6e3b143ed28f5173dd2aafe4b10ca
owenfriend24 <owenwfriend@gmail.com>
2025-04-03 12:27:23 -0500

    sample mean

---
+++ bin/sl_masks_to_func.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+#
+# take MNI clusters from SL analyses and back project to subject functional space
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage:  sl_masks_to_func.sh temple016 $FM AC $CORR"
+    exit 1
+fi
+
+sub=$1
+fmriprep_dir=$2
+comp=$3
+corr=$4
+
+mask_dir=${fmriprep_dir}/masks/sub-${sub}
+
+mkdir -p ${mask_dir}
+
+for mask in ${fmriprep_dir}/masks/sl_clusters/040325/${comp}/*; do
+
+  antsApplyTransforms -d 3 \
+      -i ${mask} \
+      -o ${mask_dir}/sl-${mask}.nii.gz \
+      -r ${corr}/freesurfer/sub-${sub}/mri/out/brainmask_func_dilated.nii.gz \
+      -t ${corr}/sub-${sub}/transforms/native_to_MNI_InverseWarp.nii.gz \
+      -t [${corr}/sub-${sub}/transforms/native_to_MNI_Affine.txt,1] \
+      -n NearestNeighbor
+
+done
