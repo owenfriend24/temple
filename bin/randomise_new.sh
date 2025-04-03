8b687261feb5baa9bca07ad42f1f585c6902b435
owenfriend24 <owenwfriend@gmail.com>
2025-04-03 09:34:06 -0500

    sample mean

--- bin/randomise_new.sh
+++ bin/randomise_new.sh
@@ -27,10 +27,20 @@ comp=$2
 #
 ##-o ${fmriprep_dir}/integration_prepost/mni_${comp}/prepost/randomise_out/grouped_age_new \
 #
-randomise -i /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_${comp}/prepost/child_group_z.nii.gz \
--o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_${comp}/prepost/randomise_out/child_parametric_demeaned_ \
--d ${fmriprep_dir}/randomise/age_param_demeaned.mat \
--t ${fmriprep_dir}/randomise/age_param_demeaned.con \
+
+randomise -i /corral-repl/utexas/prestonlab/temple/integration_prepost/mni_${comp}/prepost/group_z_image.nii.gz \
+-o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_${comp}/prepost/randomise_out/full_sample_mean \
+-d ${fmriprep_dir}/randomise/full_sample_mean.mat \
+-t ${fmriprep_dir}/randomise/full_sample_mean.con \
+-m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
+-n 5000 -x  --uncorrp
+
+
+
+randomise -i /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_ABC/prepost/group_z_image.nii.gz \
+-o /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost/mni_ABC/prepost/randomise_out/full_sample_mean \
+-d ${fmriprep_dir}/randomise/full_sample_mean.mat \
+-t ${fmriprep_dir}/randomise/full_sample_mean.con \
 -m /home1/09123/ofriend/analysis/temple/bin/templates/gm_mni/gray_17_masked.nii.gz \
 -n 5000 -x  --uncorrp
 
