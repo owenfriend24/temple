4159ceff2d2fd84fe88b44d646c6e06fe30a1eba
owenfriend24 <owenwfriend@gmail.com>
2025-04-03 12:35:28 -0500

    sample mean

--- bin/sl_masks_to_func.sh
+++ bin/sl_masks_to_func.sh
@@ -12,6 +12,44 @@ fmriprep_dir=$2
 comp=$3
 corr=$4
 
+comp=AC
+mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl-${comp}/
+
+mkdir -p ${mask_dir}
+
+for mask in ${fmriprep_dir}/masks/sl_clusters/040325/${comp}/cluster_masks/*.nii.gz; do
+  filename=$(basename "$mask")                     # e.g., "cluster1.nii.gz"
+  maskname="${filename%%.*}"
+  antsApplyTransforms -d 3 \
+      -i ${mask} \
+      -o ${mask_dir}/sl-${maskname}.nii.gz \
+      -r ${corr}/freesurfer/sub-${sub}/mri/out/brainmask_func_dilated.nii.gz \
+      -t ${corr}/sub-${sub}/transforms/native_to_MNI_InverseWarp.nii.gz \
+      -t [${corr}/sub-${sub}/transforms/native_to_MNI_Affine.txt,1] \
+      -n NearestNeighbor
+
+done
+
+comp=AB
+mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl-${comp}/
+
+mkdir -p ${mask_dir}
+
+for mask in ${fmriprep_dir}/masks/sl_clusters/040325/${comp}/cluster_masks/*.nii.gz; do
+  filename=$(basename "$mask")                     # e.g., "cluster1.nii.gz"
+  maskname="${filename%%.*}"
+  antsApplyTransforms -d 3 \
+      -i ${mask} \
+      -o ${mask_dir}/sl-${maskname}.nii.gz \
+      -r ${corr}/freesurfer/sub-${sub}/mri/out/brainmask_func_dilated.nii.gz \
+      -t ${corr}/sub-${sub}/transforms/native_to_MNI_InverseWarp.nii.gz \
+      -t [${corr}/sub-${sub}/transforms/native_to_MNI_Affine.txt,1] \
+      -n NearestNeighbor
+
+done
+
+
+comp=ABC
 mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl-${comp}/
 
 mkdir -p ${mask_dir}
