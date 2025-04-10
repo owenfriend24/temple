39b0b62dbb34cd29ceaff04c824012b23c34e5d5
Owen Friend <ofriend@login2.ls6.tacc.utexas.edu>
2025-04-10 14:33:39 -0500

    worked on moshigo compression

--- bin/compress_moshigo.sh
+++ bin/compress_moshigo.sh
@@ -9,5 +9,13 @@ fi
 
 sub=$1
 
-tar -czvf /scratch/09123/ofriend/moshi/${sub}_univariate.tar.gz \
-     /corral-repl/utexas/prestonlab/moshiGO1/${sub}/BOLD/univariate
+#tar -czvf /scratch/09123/ofriend/moshi/${sub}_univariate.tar.gz  /corral-repl/utexas/prestonlab/moshiGO1/${sub}/BOLD/univariate
+
+# to list files within a tarball: tar -tf file
+
+cp -R /corral-repl/utexas/prestonlab/moshiGO1/${sub}/BOLD/univariate /scratch/09123/ofriend/moshi/${sub}_univariate
+
+echo "copied file to scratch"
+
+# test compress and then delete a directory
+tar -czf /scratch/09123/ofriend/moshi/${sub}_univariate.tar.gz /scratch/09123/ofriend/moshi/${sub}_univariate && rm -rf /scratch/09123/ofriend/moshi/${sub}_univariate
