e621e1c7272f9a09a6e833863e6b691a704495fa
owenfriend24 <owenwfriend@gmail.com>
2025-04-13 14:49:51 -0500

    pull resid

--- bin/pull_resid.py
+++ bin/pull_resid.py
@@ -39,7 +39,7 @@ def estimate_residuals(fs_dir, fm_dir, sub):
         out_data = np.zeros([*mask_img.shape, resid.shape[0]])
         out_data[mask_img, :] = resid.T
         new_img = nib.Nifti1Image(out_data, mask_vol.affine, mask_vol.header)
-        nib.save(new_img, f'{fm_dir}/sub-{sub}/betaseries/sub-{sub}_run-{run}_resid.nii.gz')
+        nib.save(new_img, f'/scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/sub-{sub}/betaseries/sub-{sub}_run-{run}_resid.nii.gz')
         
         
 def main(fs_dir, fm_dir, sub):
