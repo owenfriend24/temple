bc084e6b9dc54e8f2975a04b2c5edeb70b288ec0
owenfriend24 <owenwfriend@gmail.com>
2025-04-15 10:56:34 -0500

    ab cubic

--- bin/temple_acf.sh
+++ bin/temple_acf.sh
@@ -10,7 +10,7 @@ fi
 fmriprep_dir="$1"
 subject="$2"
 module load afni
-out_file="${fmriprep_dir}/searchlight/sorted_acf.txt"
+out_file="${fmriprep_dir}/acf/sorted_acf.txt"
 
 for run in {1..6}; do
     output=$(3dFWHMx -mask "/corral-repl/utexas/prestonlab/temple/freesurfer/sub-${subject}/mri/out/brainmask_func_dilated.nii.gz" -ACF NULL -input "${fmriprep_dir}/sub-${subject}/betaseries/sub-${subject}_run-${run}_resid.nii.gz" -arith)
