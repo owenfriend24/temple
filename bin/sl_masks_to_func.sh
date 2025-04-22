bc62abdd24cb02bddf48c513c560b1da195ce178
owenfriend24 <owenwfriend@gmail.com>
2025-04-22 10:26:34 -0500

    group masks

--- bin/sl_masks_to_func.sh
+++ bin/sl_masks_to_func.sh
@@ -13,11 +13,11 @@ comp=$3
 corr=$4
 
 comp=AC
-mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl-${comp}_con/
+mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl_new/
 
 mkdir -p ${mask_dir}
 
-for mask in ${fmriprep_dir}/masks/sl_clusters/contrast_040325/${comp}/cluster_masks/*.nii.gz; do
+for mask in ${fmriprep_dir}/sl/cluster_masks/*.nii.gz; do
   filename=$(basename "$mask")                     # e.g., "cluster1.nii.gz"
   maskname="${filename%%.*}"
   antsApplyTransforms -d 3 \
@@ -30,39 +30,3 @@ for mask in ${fmriprep_dir}/masks/sl_clusters/contrast_040325/${comp}/cluster_ma
 
 done
 
-comp=AB
-mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl-${comp}_con/
-
-mkdir -p ${mask_dir}
-
-for mask in ${fmriprep_dir}/masks/sl_clusters/contrast_040325/${comp}/cluster_masks/*.nii.gz; do
-  filename=$(basename "$mask")                     # e.g., "cluster1.nii.gz"
-  maskname="${filename%%.*}"
-  antsApplyTransforms -d 3 \
-      -i ${mask} \
-      -o ${mask_dir}/sl-${maskname}.nii.gz \
-      -r ${corr}/freesurfer/sub-${sub}/mri/out/brainmask_func_dilated.nii.gz \
-      -t ${corr}/sub-${sub}/transforms/native_to_MNI_InverseWarp.nii.gz \
-      -t [${corr}/sub-${sub}/transforms/native_to_MNI_Affine.txt,1] \
-      -n NearestNeighbor
-
-done
-
-
-comp=ABC
-mask_dir=${fmriprep_dir}/masks/sub-${sub}/sl-${comp}_con/
-
-mkdir -p ${mask_dir}
-
-for mask in ${fmriprep_dir}/masks/sl_clusters/contrast_040325/${comp}/cluster_masks/*.nii.gz; do
-  filename=$(basename "$mask")                     # e.g., "cluster1.nii.gz"
-  maskname="${filename%%.*}"
-  antsApplyTransforms -d 3 \
-      -i ${mask} \
-      -o ${mask_dir}/sl-${maskname}.nii.gz \
-      -r ${corr}/freesurfer/sub-${sub}/mri/out/brainmask_func_dilated.nii.gz \
-      -t ${corr}/sub-${sub}/transforms/native_to_MNI_InverseWarp.nii.gz \
-      -t [${corr}/sub-${sub}/transforms/native_to_MNI_Affine.txt,1] \
-      -n NearestNeighbor
-
-done
