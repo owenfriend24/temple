c5de434579ae4676c398176b49f804869a1570ae
owenfriend24 <owenwfriend@gmail.com>
2025-04-22 12:36:08 -0500

    check moshi

--- bin/compress_moshigo.sh
+++ bin/compress_moshigo.sh
@@ -9,13 +9,16 @@ fi
 
 sub=$1
 
+count=$(find /corral-repl/utexas/prestonlab/moshiGO1/ -type f | wc -l)
+echo "$count total files"
+
 #tar -czvf /scratch/09123/ofriend/moshi/${sub}_univariate.tar.gz  /corral-repl/utexas/prestonlab/moshiGO1/${sub}/BOLD/univariate
 
 # to list files within a tarball: tar -tf file
 
-cp -R /corral-repl/utexas/prestonlab/moshiGO1/${sub}/BOLD/univariate /scratch/09123/ofriend/moshi/${sub}_univariate
-
-echo "copied file to scratch"
-
-# test compress and then delete a directory
-tar -czf /scratch/09123/ofriend/moshi/${sub}_univariate.tar.gz /scratch/09123/ofriend/moshi/${sub}_univariate && rm -rf /scratch/09123/ofriend/moshi/${sub}_univariate
+#cp -R /corral-repl/utexas/prestonlab/moshiGO1/${sub}/BOLD/univariate /scratch/09123/ofriend/moshi/${sub}_univariate
+#
+#echo "copied file to scratch"
+#
+## test compress and then delete a directory
+#tar -czf /scratch/09123/ofriend/moshi/${sub}_univariate.tar.gz /scratch/09123/ofriend/moshi/${sub}_univariate && rm -rf /scratch/09123/ofriend/moshi/${sub}_univariate
