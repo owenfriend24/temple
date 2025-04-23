f97301893bb90d692d476567faafde61647abe1d
owenfriend24 <owenwfriend@gmail.com>
2025-04-22 19:32:39 -0500

    batch

--- bin/batch_agg.sh
+++ bin/batch_agg.sh
@@ -5,11 +5,12 @@ comps=$1
 
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
-aggregate_integration.py symmetry /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AB searchlight --agg_file
-#
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-BC searchlight --agg_file
+AB searchlight --agg_file
+
+#
+#aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
+#BC searchlight --agg_file
 #
 
 #aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
--- bin/batch_agg3.sh
+++ bin/batch_agg3.sh
@@ -6,4 +6,4 @@ comps=$1
 source /home1/09123/ofriend/analysis/temple/rsa/bin/activate
 
 aggregate_integration.py both /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/integration_prepost \
-AC searchlight --agg_file
+BC searchlight --agg_file
