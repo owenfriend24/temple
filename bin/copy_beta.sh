7d097a047d3ecb230428a4f19e41296c70fef2d3
owenfriend24 <owenwfriend@gmail.com>
2025-04-22 19:37:28 -0500

    batch

--- bin/copy_beta.sh
+++ bin/copy_beta.sh
@@ -5,4 +5,4 @@ sub=$1
 
 rm -R /corral-repl/utexas/prestonlab/temple/sub-${sub}/betaseries
 
-cp -R /scratch/09123/ofriend/temple/new_prepro/fmriprep/derivatives/sub-${sub}/betaseries /corral-repl/utexas/prestonlab/temple/sub-${sub}/
+cp -R /scratch/09123/ofriend/temple/new_prepro/derivatives/fmriprep/sub-${sub}/betaseries /corral-repl/utexas/prestonlab/temple/sub-${sub}/
