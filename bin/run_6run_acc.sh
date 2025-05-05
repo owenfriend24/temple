3bcce5189dc91792ec044a52334e44ce5ad0490d
owenfriend24 <owenwfriend@gmail.com>
2025-05-05 17:49:33 -0500

    factor run

---
+++ bin/run_6run_acc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sub=$1
+
+source $HOME/analysis/temple/rsa/bin/activate
+python $HOME/analysis/moshigo_model/bin/voxelwise_parallel_6run_acc.py --chunk ${sub}
+
+#python $HOME/analysis/moshigo_model/bin/pca_cluster_model.py
