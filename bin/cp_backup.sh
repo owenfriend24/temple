8c16a5b3c7c32347bf15d0b88e336895f8e0d3e1
Owen Friend <ofriend@login2.ls6.tacc.utexas.edu>
2025-05-06 21:02:20 -0500

    m

---
+++ bin/cp_backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp $SCRATCH/moshi/pca_sl/results/group_Adults_by_run_1minuspmap_FULL_6rfactor.nii.gz $HOME/
+cp $SCRATCH/moshi/pca_sl/results/group_Adults_by_run_1minuspmap_FULL_6rfactor_acc.nii.gz $HOME/
