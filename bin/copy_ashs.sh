fd4723042f35cc659a6369a65d2cfb9a73a4fbfa
owenfriend24 <owenwfriend@gmail.com>
2025-05-22 15:47:46 -0500

    rois for tsnr testing

--- bin/copy_ashs.sh
+++ bin/copy_ashs.sh
@@ -6,15 +6,20 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-corr=$1
+sub=$1
+
+mkdir -p $CORR/ashs/masks/sub-${sub}
+
+cp -R $SCRATCH/ashs/new_test/sub-${sub}/final $CORR/ashs/masks/sub-${sub}/
+cp -R $SCRATCH/ashs/new_test/sub-${sub}/subfield_masks $CORR/ashs/masks/sub-${sub}/
 
 
 
-cp -R $SCRATCH/ashs/bin $CORR/ashs/
-cp $SCRATCH/ashs/*.txt $CORR/ashs/
-cp -R $SCRATCH/ashs/ext $CORR/ashs/
-cp -R $SCRATCH/ashs/src $CORR/ashs/
-cp -R $SCRATCH/ashs/test_atlas $CORR/ashs/
-cp $SCRATCH/ashs/*.md $CORR/ashs/
-cp -R $SCRATCH/ashs/submodules $CORR/ashs/
-cp $SCRATCH/ashs/LICENSE $CORR/ashs
+#cp -R $SCRATCH/ashs/bin $CORR/ashs/
+#cp $SCRATCH/ashs/*.txt $CORR/ashs/
+#cp -R $SCRATCH/ashs/ext $CORR/ashs/
+#cp -R $SCRATCH/ashs/src $CORR/ashs/
+#cp -R $SCRATCH/ashs/test_atlas $CORR/ashs/
+#cp $SCRATCH/ashs/*.md $CORR/ashs/
+#cp -R $SCRATCH/ashs/submodules $CORR/ashs/
+#cp $SCRATCH/ashs/LICENSE $CORR/ashs
