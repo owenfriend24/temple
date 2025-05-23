1c322727ffecec012ee498180d1c05d3a460b204
owenfriend24 <owenwfriend@gmail.com>
2025-05-23 10:45:09 -0500

    updated univariate/ppi run functions

--- bin/edit_first_uni.sh
+++ bin/edit_first_uni.sh
@@ -29,19 +29,19 @@ d2=$(fslinfo "$nifti_file1" | awk '$1 == "dim2" {print $2}')
 d3=$(fslinfo "$nifti_file1" | awk '$1 == "dim3" {print $2}')
 
 
-nifti_file1=$fm_dir/sub-"$subject"/func/skullstripped_T1/sub-"$subject"_task-collector_run-01_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
+nifti_file1=$fm_dir/sub-"$subject"/func/sub-"$subject"_task-collector_run-01_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
 num_vols1=$(fslinfo "$nifti_file1" | awk '$1 == "dim4" {print $2}')
 num_vox1=$((num_vols1*d1*d2*d3))
 
-nifti_file2=$fm_dir/sub-"$subject"/func/skullstripped_T1/sub-"$subject"_task-collector_run-02_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
+nifti_file2=$fm_dir/sub-"$subject"/func/sub-"$subject"_task-collector_run-02_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
 num_vols2=$(fslinfo "$nifti_file2" | awk '$1 == "dim4" {print $2}')
 num_vox2=$((num_vols2*d1*d2*d3))
 
-nifti_file3=$fm_dir/sub-"$subject"/func/skullstripped_T1/sub-"$subject"_task-collector_run-03_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
+nifti_file3=$fm_dir/sub-"$subject"/func/sub-"$subject"_task-collector_run-03_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
 num_vols3=$(fslinfo "$nifti_file3" | awk '$1 == "dim4" {print $2}')
 num_vox3=$((num_vols3*d1*d2*d3))
 
-nifti_file4=$fm_dir/sub-"$subject"/func/skullstripped_T1/sub-"$subject"_task-collector_run-04_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
+nifti_file4=$fm_dir/sub-"$subject"/func/sub-"$subject"_task-collector_run-04_space-T1w_desc-preproc_bold_ss_4mm.nii.gz
 num_vols4=$(fslinfo "$nifti_file4" | awk '$1 == "dim4" {print $2}')
 num_vox4=$((num_vols4*d1*d2*d3))
 
