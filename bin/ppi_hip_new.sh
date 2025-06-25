4133b5c23fc25e7e1b50db1feecdf38ba9e68613
owenfriend24 <owenwfriend@gmail.com>
2025-06-25 13:46:42 -0500

    updated univariate/ppi run functions

--- bin/ppi_hip_new.sh
+++ bin/ppi_hip_new.sh
@@ -15,6 +15,5 @@ mkdir -p ${output_dir}/sub-${subject}/ppi_inverse
 # mask functional data for HPC roi
 for run in 1 2 3 4; do
     fslmeants -i /corral-repl/utexas/prestonlab/temple/sub-${subject}/func/sub-${subject}_task-${task}_run-0${run}_space-T1w_desc-preproc_bold_ss_4mm.nii.gz \
-    -m /corral-repl/utexas/prestonlab/temple/sub-${subject}/masks/hip_masks/func-b_hip.nii.gz --eig -o ${output_dir}/sub-${subject}/ppi/run-${run}_eigen_hip.txt;
-    cp ${output_dir}/sub-${subject}/ppi/run-${run}_eigen_hip.txt ${output_dir}/sub-${subject}/ppi_inverse/run-${run}_eigen_hip.txt
+    -m /corral-repl/utexas/prestonlab/temple/sub-${subject}/masks/hip_masks/func-b_hip.nii.gz --eig -o ${output_dir}/sub-${subject}/ppi/run-${run}_eigen_hip.txt
 done
