276f2378da6d1128192cc1acf085175920e5589a
owenfriend24 <owenwfriend@gmail.com>
2025-06-27 13:55:54 -0500

    updated univariate/ppi run functions

--- bin/batch_ppi.sh
+++ bin/batch_ppi.sh
@@ -16,8 +16,8 @@ corr=/corral-repl/utexas/prestonlab/temple
 source $HOME/analysis/temple/profile
 
 # create behavioral files for both analyses
-ppi_hip_new.sh ${fmriprep_dir}/ppi/ ${sub} collector
-ppi_txt_behav.py ${corr} both ${sub} ${sub_dir}/ppi/
+/home1/09123/ofriend/analysis/temple/bin/ppi_hip_new.sh ${fmriprep_dir}/ppi/ ${sub} collector
+/home1/09123/ofriend/analysis/temple/bin/ppi_txt_behav.py ${corr} both ${sub} ${sub_dir}/ppi/
 
 # rename any previous analyses (should generally go in and delete to save space)
 for dir in ppi ppi_inverse; do
@@ -29,15 +29,15 @@ for dir in ppi ppi_inverse; do
 done
 
 # create .fsf files for increased connectivity at triplet boundaries
-edit_first_ppi.sh /home1/09123/ofriend/analysis/temple/univ/ppi_first_template.fsf ${sub_dir}/ppi/ ${sub} ${corr}
-run_first_ppi.sh ${fmriprep_dir} ${sub} ${corr}
-edit_second_ppi.sh /home1/09123/ofriend/analysis/temple/univ/2nd_level_ppi_template.fsf ${sub_dir}/ppi/ ${sub} ppi
-run_second_ppis.sh ${fmriprep_dir}/ppi/ ${sub} ppi
+/home1/09123/ofriend/analysis/temple/bin/edit_first_ppi.sh /home1/09123/ofriend/analysis/temple/univ/ppi_first_template.fsf ${sub_dir}/ppi/ ${sub} ${corr}
+/home1/09123/ofriend/analysis/temple/bin/run_first_ppi.sh ${fmriprep_dir} ${sub} ${corr}
+/home1/09123/ofriend/analysis/temple/bin/edit_second_ppi.sh /home1/09123/ofriend/analysis/temple/univ/2nd_level_ppi_template.fsf ${sub_dir}/ppi/ ${sub} ppi
+/home1/09123/ofriend/analysis/temple/bin/run_second_ppis.sh ${fmriprep_dir}/ppi/ ${sub} ppi
 
 # create .fsf files for DECREASED connectivity at triplet boundaries
-edit_first_ppi.sh /home1/09123/ofriend/analysis/temple/univ/ppi_first_inverse.fsf ${sub_dir}/ppi_inverse/ ${sub} ${corr}
-run_first_ppi_inverse.sh ${fmriprep_dir} ${sub} ${corr}
-edit_second_ppi.sh /home1/09123/ofriend/analysis/temple/univ/2nd_level_ppi_template.fsf ${sub_dir}/ppi_inverse/ ${sub} inverse
-run_second_ppis.sh ${fmriprep_dir}/ppi/ ${sub} inverse
+/home1/09123/ofriend/analysis/temple/bin/edit_first_ppi.sh /home1/09123/ofriend/analysis/temple/univ/ppi_first_inverse.fsf ${sub_dir}/ppi_inverse/ ${sub} ${corr}
+/home1/09123/ofriend/analysis/temple/bin/run_first_ppi_inverse.sh ${fmriprep_dir} ${sub} ${corr}
+/home1/09123/ofriend/analysis/temple/bin/edit_second_ppi.sh /home1/09123/ofriend/analysis/temple/univ/2nd_level_ppi_template.fsf ${sub_dir}/ppi_inverse/ ${sub} inverse
+/home1/09123/ofriend/analysis/temple/bin/run_second_ppis.sh ${fmriprep_dir}/ppi/ ${sub} inverse
 
 
