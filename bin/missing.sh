d4af15463213b5784743ac41624ee374726a054a
owenfriend24 <owenwfriend@gmail.com>
2025-08-07 10:15:08 -0500

    missing sub function

--- bin/missing.sh
+++ bin/missing.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $# -lt 2 ]]; then
+    echo "missing.sh subjects (colon-separated) search_dir"
+    exit 1
+fi
+
 subjects=$1       # colon-separated subject list
 search_dir=$2     # directory to search in
 
