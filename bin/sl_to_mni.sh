64fad6bf8af21e683bdf66c7bd0bdb11215ce6cd
owenfriend24 <owenwfriend@gmail.com>
2025-08-07 10:25:43 -0500

    missing sub function

--- bin/sl_to_mni.sh
+++ bin/sl_to_mni.sh
@@ -3,7 +3,7 @@
 # transform searchlight outputs to template space for across participant comparisons
 
 if [[ $# -lt 1 ]]; then
-    echo "Usage:  sl_to_mni.sh sub fmriprep_dir comp roi measure (e.g., sl_to_mni.sh temple024 FM AB brainmask_func_dilated symmetry)"
+    echo "Usage:  sl_to_mni.sh sub fmriprep_dir comp roi measure (e.g., sl_to_mni.sh temple024 FM AB b_gray_func prepost)"
     exit 1
 fi
 
@@ -12,6 +12,7 @@ fmriprep_dir=$2
 comp=$3
 roi=$4
 measure=$5
+
 mkdir -p ${fmriprep_dir}/integration_prepost/mni_${comp}/${measure}
 #WarpImageMultiTransform 3 "${fmriprep_dir}/searchlight/prepost_${comp}_symm/${sub}_symm_${roi}_z.nii.gz" "${fmriprep_dir}/searchlight/mni_${comp}_symm/${sub}_${comp}_symm_${roi}_mni.nii.gz" -R "/home1/09123/ofriend/analysis/temple/bin/templates/MNI152_T1_func_brain.nii.gz" "${fmriprep_dir}/sub-${sub}/transforms/native_to_MNI_Warp.nii.gz" "${fmriprep_dir}/sub-${sub}/transforms/native_to_MNI_Affine.txt"
 
