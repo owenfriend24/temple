63c9ec4bda1b960b025145bfc97de31a70bdd204
owenfriend24 <owenwfriend@gmail.com>
2025-08-18 15:29:02 -0500

    acf function for timeseries analyses

--- bin/temple_acf.sh
+++ bin/temple_acf.sh
@@ -12,7 +12,7 @@ fmriprep_dir="$3"
 roi="$4"
 
 module load afni
-out_file="${corral}/clust_sim/acf/by_subject_run_${roi}_acf.txt"
+
 for roi in 'gm' 'b_hip' 'b_hip_ant'; do
   if [[ $roi == 'gm' ]]; then
           mask_path="${corral}/freesurfer/sub-${subject}/mri/b_gray_func.nii.gz"
@@ -37,6 +37,9 @@ for roi in 'gm' 'b_hip' 'b_hip_ant'; do
           echo "Skipping subject ${subject} run ${run}"
           continue
       fi
+
+      out_file="${corral}/clust_sim/acf/by_subject_run_${roi}_acf.txt"
+
       output=$(3dFWHMx -mask ${mask_path} -ACF NULL -input "${fmriprep_dir}/residuals/sub-${subject}/sub-${subject}_run-${run}_resid.nii.gz" -arith)
       acf_coefs=$(echo "$output" | tail -n 1 | awk '{print $(NF-3), $(NF-2), $(NF-1), $NF}')
       echo "$subject $run $acf_coefs" >> "$out_file"
