fe9a5f2e5d9f3c8f86d5935df2ac7597047ec6e2
owenfriend24 <owenwfriend@gmail.com>
2025-10-16 18:12:23 -0500

    try rerunning fs

--- bin/run_first_feats.sh
+++ bin/run_first_feats.sh
@@ -21,7 +21,7 @@ if [ "$type" == "boundary" ]; then
     roi_tag=""
 elif [ "$type" == "boundary_inverse" ]; then
     base=${fmriprep_dir}/sub-${subject}/univ/univ_inverse/
-    fsf_base=${corral}/sub-${subject}/univ/univ_inverse/
+    fsf_base=${corral}/sub-${subject}/univ/
     roi_tag=""
 elif [ "$type" == "ppi" ]; then
     base=${fmriprep_dir}/sub-${subject}/univ/ppi/
