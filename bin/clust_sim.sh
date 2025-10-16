ecd750c1a2b4f70249871d5d63fb41099460f972
owenfriend24 <owenwfriend@gmail.com>
2025-10-15 20:35:07 -0500

    try rerunning fs

--- bin/clust_sim.sh
+++ bin/clust_sim.sh
@@ -12,12 +12,19 @@ fmriprep_dir=$1
 module load afni
 export OMP_NUM_THREADS=None
 
+## RSA
+#cd /corral-repl/utexas/prestonlab/temple/clust_sim/gm
+#3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/gm/gm_binary/prob_mask.nii.gz -acf 0.498 2.413 8.441 -nodec -prefix gm_50
+#
+#cd /corral-repl/utexas/prestonlab/temple/clust_sim/b_hip
+#3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/hip_func/b_hip_func.nii.gz -acf 0.878 2.489 1.873 -nodec -prefix hip_full
+#
+#cd /corral-repl/utexas/prestonlab/temple/clust_sim/b_hip_ant
+#3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/hip_func/b_hip_ant_func.nii.gz -acf 0.975 2.298 0.931 -nodec -prefix hip_ant
+
+# UNIV-BOUNDARY
 cd /corral-repl/utexas/prestonlab/temple/clust_sim/gm
-3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/gm/gm_binary/prob_mask.nii.gz -acf 0.498 2.413 8.441 -nodec -prefix gm_50
+3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/gm/gm_binary/prob_mask.nii.gz -acf 0.520 2.405 8.276 -nodec -prefix gm_50_univ_boundary
 
 cd /corral-repl/utexas/prestonlab/temple/clust_sim/b_hip
-3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/hip_func/b_hip_func.nii.gz -acf 0.878 2.489 1.873 -nodec -prefix hip_full
-
-cd /corral-repl/utexas/prestonlab/temple/clust_sim/b_hip_ant
-3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/hip_func/b_hip_ant_func.nii.gz -acf 0.975 2.298 0.931 -nodec -prefix hip_ant
-
+3dClustSim -mask /corral-repl/utexas/prestonlab/temple/group_masks/hip_func/b_hip_func.nii.gz -acf 0.859 2.546 2.125 -nodec -prefix hip_full_univ_boundary
